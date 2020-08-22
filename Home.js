d39d43bac0e0a4e32bed1a488f98b3752cd863e9
promila1237 <69894174+promila1237@users.noreply.github.com>
2020-08-22 13:01:10 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,362 @@
+import React from 'react';
+import siteimage from '../src/siteimage.jpg';
+import siteimage1 from '../src/siteimage1.jpg';
+import siteimage2 from '../src/siteimage2.jpg';
+import siteimage3 from '../src/siteimage3.jpg';
+import sitelogo from '../src/sitelogo.png';
+import Navbar from './Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+import { BsSearch } from 'react-icons/bs'
+import siteimage4 from '../src/siteimage4.jpg';
+import siteimage5 from '../src/siteimage5.jpg';
+import siteimage7 from '../src/siteimage7.jpg';
+import siteimage6 from '../src/siteimage6.png';
+import siteimage8 from '../src/siteimage8.jpg';
+import siteimage9 from '../src/siteimage9.jpg';
+import siteimage10 from '../src/siteimage10.jpg';
+import siteimage11 from '../src/siteimage11.jpg';
+import siteimage12 from '../src/siteimage12.jpg';
+import siteimage13 from '../src/siteimage13.jpg';
+import siteimage14 from '../src/siteimage14.jpg';
+import siteimage15 from '../src/siteimage15.jpg';
+import siteimage16 from '../src/siteimage16.png';
+import siteimage17 from '../src/siteimage17.png';
+import siteimage18 from '../src/siteimage18.png';
+import siteimage19 from '../src/siteimage19.png';
+import siteimage20 from '../src/siteimage20.png';
+
+class Web extends React.Component{
+      render(){
+      return(
+       
+          <div>
+              <div className="row">
+                 <div className="col-3">
+                 <a href="https://suryaurza.com/">
+						
+                         <img src={sitelogo} alt="solar penal"/>
+                      
+                         </a>
+                         </div>
+                         <div className="col-6">
+                           < Navbar/>
+                         </div>
+                         <div className="col-3">
+                          <h6>Call Us 9050438486</h6>
+                          <BsSearch/>
+                         </div>
+
+                  </div>
+              
+
+                 <div>
+                 <img  height="500px" width="1350px"src = {siteimage} alt="make"/>
+                 </div>
+
+                  <div>
+                  <h2 style={{textAlign:"center", marginTop:"5%",marginBottom:"5%"}}> Solar Modules </h2>
+                  
+
+                   <div class="row">
+                   <div class="col-lg-4 ">
+                       <a href="https://suryaurza.com/product/su-kam-solar-panel/">
+                         <img  src={siteimage1} alt="solar penal" className="penal"/>
+                      </a>
+                      <h6>SU-Kam Solar Prenal</h6>
+                    </div>
+            
+                    <div class="col-lg-4">
+                       <a href="https://suryaurza.com/product/su-kam-solar-panel/">
+                         <img src={siteimage1} alt="solar penal" className="penal"/>
+                      </a>
+                      <h6>Alpex Solar Penal</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                       <a href="https://suryaurza.com/product/su-kam-solar-panel/">
+                         <img src={siteimage1} alt="solar penal" className="penal"/>
+                      </a>
+                      <h6>Bluebird Solar Penal</h6>
+                    </div>
+                    </div>  
+
+                 </div>
+ 
+                <div>
+                  <h2 style={{textAlign:"center" , marginTop:"5%",marginBottom:"5%"}}>About Solar</h2>
+                  <p><b>Solar</b>: Relating to or denoting energy derived from the sun’s rays: “solar heating”.Solar energy begins with the sun.
+                   Solar panels, also known as photovoltaics, are used to convert light from the sun, which is composed of particles of energy called
+                   “photons”, into electricity that can be used to power
+                   electrical loads.</p>
+                  <p> 
+                  <b>Why Go Solar?</b></p>
+                  <p>The sun is a clean, renewable source of energy.With rising electricity prices, climate change concerns and a growing demand for electricity
+                  ,renewable energy resources are becoming an increasingly valuable and necessary part of the world’s energy mix.If you’ve invested in solar
+                   power for your property then you’re probably sitting back and enjoying the benefits! Unfortunately solar panels do require maintenance, 
+                   something you may not have realized until you were researching; many people aren’t aware that solar panels to require specific care. One
+                  of the easiest and least expensive ways to maintain the health of your solar panels is through cleaning. Simple cleaning procedures will
+                  ensure that your solar panels are in the best shape possible.</p>
+
+
+
+
+                
+                <a href= "https://suryaurza.com/product/sukam-grid-tie-inverter/">SuKam Grid Tie Inverter
+                <img src={siteimage2}  alt="solar penal"/>
+                </a>
+
+                <a href= "https://suryaurza.com/product/sukam-solar-pcu/">SuKam Solar PCU
+                <img src={siteimage3}  alt="solar penal"/>
+                </a>
+                </div>
+
+
+
+                <div>
+                <h2 style={{textAlign:"center", marginTop:"5%",marginBottom:"5%"}}>Charge Controller</h2>
+
+
+                <div class="row" style={{marginleft:"5%"}}>
+                   <div class="col-lg-3">
+                       <a href="https://suryaurza.com/product/2143/">
+                         <img src={siteimage4} alt="solar penal" className="solarpenal"/>
+                       </a>
+                    <h6>12-24-48 Volt-45AMP</h6>
+                  </div>
+            
+                    <div class="col-lg-3">
+                       <a href="https://suryaurza.com/product/12volt10-amp/">
+                        <img src={siteimage5} alt="solar penal" className="solarpenal"/>
+                        </a>
+                        <h6>12Volt/10amp Charge Controler</h6>
+                    </div>
+
+                    <div class="col-lg-3">
+                       <a href="https://suryaurza.com/product/12-24-volt-20amp-solar-con/">                          
+                         <img src={siteimage7} alt="solar penal" className="solarpenal"/>
+                      </a>
+                      <h6>12-24Volt/20amp Solar Con</h6>
+                    </div>
+                    <div class="col-lg-3">
+                       <a href="https://suryaurza.com/product/120-volt60-amp-charge-controller/">
+                       <img src={siteimage6} alt="solar penal" className="solarpenal"/>
+                      </a>
+                      <h6>120Volt/60amp Charge Controler</h6>
+                    </div>
+                  </div>
+
+                </div>
+
+
+                <div>
+                <h2 style={{textAlign:"center", marginTop:"5%", marginBottom:"5%"}}>Solar Ready To Fix</h2>
+
+
+            <div class="row">
+            <div class="col-lg-3">
+            <a href="https://suryaurza.com/product/300-watt-for-single-battery-inverter/">
+             <img src={siteimage8} alt="solar penal" className="solarpenal"/>
+           </a>
+            <h6>300 watt for single battery <br/>inverter</h6>
+            </div>
+
+          <div class="col-lg-3">
+          <a href="https://suryaurza.com/product/600watt-for-single-battery-inverter/">
+          <img src={siteimage8} alt="solar penal" className="solarpenal"/>
+         </a>
+         <h6>600 watt for single battery <br/>inverter</h6>
+        </div>
+
+         <div class="col-lg-3">
+         <a href="https://suryaurza.com/product/1kw-complete-system/">                          
+         <img src={siteimage9} alt="solar penal" className="solarpenal"/>
+         </a>
+         <h6>1KW complete system</h6>
+        </div>
+        <div class="col-lg-3">
+        <a href="https://suryaurza.com/product/2kw-complete-system/">
+        <img src={siteimage9} alt="solar penal" className="solarpenal" />
+        
+       </a>
+        <h6>2KW complete system</h6>
+       </div>
+      </div>
+
+
+      </div>
+
+
+
+
+    
+        <div>
+            <h2 style={{textAlign:"center", marginTop:"5%", marginBottom:"5%"}}>Solar Ready To Fix</h2>
+
+
+        <div class="row">
+        <div class="col-lg-4">
+        <a href="https://suryaurza.com/product/invt-36months-165ah/">
+         <img src={siteimage10} alt="solar penal" className="solar"/>
+         </a>
+         <h6>INVT-36 MONTHS 165AH</h6>
+          </div>
+
+         <div class="col-lg-4">
+          <a href="https://suryaurza.com/product/int-24months-170ah/">
+        <img src={siteimage11} alt="solar penal" className="solar"/>
+        </a>
+        <h6>INT-24 MONTHS 170Ah</h6>
+       </div>
+
+         <div class="col-lg-4">
+       <a href="https://suryaurza.com/product/inmt-24months-180ah/">                          
+         <img src={siteimage12} alt="solar penal" className="solar"/>
+         </a>
+        <h6>INT-24 MONTHS 180Ah</h6>
+      </div>
+    
+      </div>
+      </div>
+
+
+
+
+      <div>
+         <div class="row">
+        <div class="col-lg-4">
+        <a href="https://suryaurza.com/product/inverter-950sg/">
+         <img src={siteimage13} alt="solar penal" className="solar"/>
+         </a>
+         <h6>Inverter 950SG</h6>
+          </div>
+
+         <div class="col-lg-4">
+          <a href="https://suryaurza.com/product/inverter-1200sg/">
+        <img src={siteimage14} alt="solar penal" className="solar"/>
+        </a>
+        <h6>Inverter 1200SG</h6>
+       </div>
+
+         <div class="col-lg-4">
+       <a href="https://suryaurza.com/product/inverter-1650sg/">                          
+         <img src={siteimage15} alt="solar penal" className="solar"/>
+         </a>
+        <h6>Inverter 1650SG</h6>
+      </div>
+    
+      </div>
+      </div>
+
+
+
+      
+      <div>
+         <div class="row" style={{backgroundColor:"#333"}}>
+        <div class="col-lg-3">
+          <h4>SURYAURZA</h4>
+          <p>Surya Urza is a fully integrated<br/>
+             solar solutions provider from<br/>
+             wafer to cell, module, subsystems,<br/>
+             systems, installation,<br/>
+             commissioning, maintenance and<br/>
+             service.</p>
+             <a class="read_more" href="https://suryaurza.com/about-us/">Read More</a>
+       
+          </div>
+
+         <div class="col-lg-3">
+         <h4>CONTACT</h4>
+         <p>Surya Urza Enterprises<br/>
+            138 MC Colony<br/>
+            Hisar – 125005<br/>
+            Haryana<br/>
+           
+
+            amit@suryaurza.com<br/>
+
+            +91 9050438486<br/>
+            +91 7357810717 </p>
+        
+       </div>
+
+         <div class="col-lg-3">
+         <h4>SOLAR PANEL SYSTEM</h4>
+         <div>
+         <a href="https://suryaurza.com/photovoltaic-solar-panels/">Photovoltaic Solar Panels</a>
+         </div>
+
+         <div>
+         <a href="https://suryaurza.com/renewable-energy/">What is Renewable Energy?</a>
+         </div>
+
+         <div>
+         <a href="https://suryaurza.com/what-size-solor-system-work-need/">What Size System Do I Need?</a>
+         </div>
+
+
+         <div>
+         <a href="https://suryaurza.com/solar-panel-maintenance/">Solar Panel Maintenance</a>
+         </div>
+
+         <div>
+         <a href="https://suryaurza.com/how-do-solar-panels-work/">How Do Solar Panels Work?</a>
+         </div>
+
+
+
+       
+      </div>
+
+      <div class="col-lg-3">
+       <h4>CONNECT WITH US</h4>
+       
+       <a  href="https://plus.google.com/u/0/113586752976894780578/" >
+         <img src={siteimage16} alt="solar penal"/>
+         </a>
+
+         <a href="https://www.instagram.com/suryaurzaenterprises/">
+         <img src={siteimage17} alt="solar penal"/>
+         </a>
+         
+          <a href="https://twitter.com/suryaurza1">
+         <img src={siteimage18} alt="solar penal"/>
+         </a>
+         
+         <a href="https://www.facebook.com/suryaurza.hisar">
+         <img src={siteimage19} alt="solar penal"/>
+         </a>
+         
+         <a href="https://www.facebook.com/suryaurza.hisar">
+         <img src={siteimage20} alt="solar penal"/>
+         </a>
+         
+       
+           </div>
+          </div>
+      </div>
+
+
+      <div style={{textAlign:"center", backgroundColor:"black"}}>
+      <a href="https://suryaurza.com/privacy-policy/">Privacy Policy</a>
+       |
+       <a href="https://suryaurza.com/term-and-conditions/">Terms & Condition</a>
+         <p>© Copyright 2020 SuryaUrza.</p>
+
+      </div>
+
+
+
+
+
+
+
+             
+
+        </div>
+     )
+   }
+}
+
+export default Web;
